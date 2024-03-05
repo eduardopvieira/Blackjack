20c13b420f35f416784d5cd5921f55bf12e7131f
paulovictor181 <paulo_victor_cc82@outlook.com>
2024-03-05 09:23:37 -0300

    Commit para mostrar as cartas no terminal

--- BlackJack/src/Classes/Mao.java
+++ BlackJack/src/Classes/Mao.java
@@ -29,12 +29,113 @@ public class Mao {
         this.listaCartas.add(carta);
     }
 
+    private String conversorNaipe(Carta carta){
+
+        String Naipe = "";
+
+        switch (carta.getNaipe()) {
+            case "Copas":
+                Naipe = "\u2665";
+                System.out.println(Naipe);
+                break;
+            case "Paus":
+                Naipe = "\u2665";
+                System.out.println(Naipe);
+                break;
+            case "Espadas":
+                Naipe = "\u2665";
+                System.out.println(Naipe);
+                break;
+            case "Ouros":
+                Naipe = "\u2665";
+                System.out.println(Naipe);
+                break;
+            default:
+                break;
+        }
+
+        return Naipe;
+    }
+
+    private String conversorNumero(Carta carta){
+
+        String numero = "";
+
+        switch (carta.getValor()) {
+            case 1:
+                numero = "A";
+                break;
+            case 2:
+                numero = "2";
+                break;
+            case 3:
+                numero = "3";
+                break;
+            case 4:
+                numero = "4";
+                break;
+            case 5:
+                numero = "5";
+                break;
+            case 6:
+                numero = "6";
+                break;
+            case 7:
+                numero = "7";
+                break;
+            case 8:
+                numero = "8";
+                break;
+            case 9:
+                numero = "9";
+                break;
+            case 10:
+                numero = "10";
+                break;
+            case 11:
+                numero = "J";
+                break;
+            case 12:
+                numero = "Q";
+                break;
+            case 13:
+                numero = "K";
+                break;
+            default:
+                break;
+        }
+
+        return numero;
+    }
+
     public void mostraMao(){
         System.out.println("Sua mão atual é:");
 
+        String maoFinal = "";
+
+        String linha1 = "";
+        String linha2 = "";
+        String linha3 = "";
+        String linha4 = "";
+
         for (Carta carta : listaCartas) {
-            System.out.println(carta.getValor() + " de " + carta.getNaipe());
+
+            String naipe = conversorNaipe(carta);
+            String numero = conversorNumero(carta);
+
+            linha1 += " _____  ";
+            if(numero == "10"){
+                linha2 += "|"+ numero +"   | ";
+            } else {
+                linha2 += "|"+ numero +"    | ";
+            }
+            linha3 += "|  "+ naipe +"  | ";
+            linha4 += "|_____| ";
         }
+
+        maoFinal = linha1 + "\n" + linha2 + "\n" + linha3 + "\n" + linha4 + "\n"; 
+
+        System.out.println(maoFinal);
     }
 
 
