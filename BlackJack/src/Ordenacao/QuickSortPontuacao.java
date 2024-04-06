67ecf803d7da06a8c8dfa789ec60ef24ee74e7dc
Eduardo <eduardopazvieiraa@gmail.com>
2024-04-06 16:19:39 -0300

    adicionando botao de ranking e algoritmo de ordenacao

---
+++ BlackJack/src/Ordenacao/QuickSortPontuacao.java
@@ -0,0 +1,46 @@
+package Ordenacao;
+import java.util.ArrayList;
+import Classes.Pontuacao;
+
+public class QuickSortPontuacao {
+
+    public void sort(ArrayList<Pontuacao> pontuacoes) {
+        if (pontuacoes == null || pontuacoes.size() == 0) {
+            return;
+        }
+        quicksort(pontuacoes, 0, pontuacoes.size() - 1);
+    }
+
+    private void quicksort(ArrayList<Pontuacao> pontuacoes, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(pontuacoes, low, high);
+            quicksort(pontuacoes, low, pivotIndex - 1);
+            quicksort(pontuacoes, pivotIndex + 1, high);
+        }
+    }
+
+    private int partition(ArrayList<Pontuacao> pontuacoes, int low, int high) {
+        double pivot = pontuacoes.get(high).getTempo();
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (pontuacoes.get(j).getTempo() <= pivot) {
+                i++;
+                swap(pontuacoes, i, j);
+            }
+        }
+        swap(pontuacoes, i + 1, high);
+        return i + 1;
+    }
+
+    private void swap(ArrayList<Pontuacao> pontuacoes, int i, int j) {
+        Pontuacao temp = pontuacoes.get(i);
+        pontuacoes.set(i, pontuacoes.get(j));
+        pontuacoes.set(j, temp);
+    }
+
+    public void printPontuacoes(ArrayList<Pontuacao> pontuacoes) {
+        for (Pontuacao pontuacao : pontuacoes) {
+            System.out.println(pontuacao);
+        }
+    }
+}
